d50cbe635840e570c2706d5da35c0e68fbaf11b2
gaddganepola <ganeypola@gmail.com>
2025-09-13 11:37:46 +0530

    create the product model

---
+++ src/main/java/com/ddg/MongoDBDemo/model/Product.java
@@ -0,0 +1,22 @@
+package com.ddg.MongoDBDemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    private String id;
+    private String productName;
+    private double price;
+    private int quantity;
+    private String description;
+
+}
