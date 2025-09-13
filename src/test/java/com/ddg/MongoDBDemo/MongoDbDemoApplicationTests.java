0cd234d53b747ca53528f1cf988fc723d9532331
gaddganepola <ganeypola@gmail.com>
2025-09-13 10:48:38 +0530

    project initialization

---
+++ src/test/java/com/ddg/MongoDBDemo/MongoDbDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddg.MongoDBDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
